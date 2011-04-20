00b3a4f6f6a04501b551773a14644eb6b37e4654
hongli li <anotherdayinpara@yahoo.com>
2011-04-19 21:31:56 -0400

    added basic sms

--- src/iching/android/activities/CastIChing.java
+++ src/iching/android/activities/CastIChing.java
@@ -635,20 +635,6 @@ public class CastIChing extends Activity implements OnClickListener, SensorEvent
 	private void vibrate(long[] pattern)
 	{
 		Vibrator vibrator = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
-		/*	int dot = 200;		// Length of a Morse Code "dot" in milliseconds
-						int dash = 500;		// Length of a Morse Code "dash" in milliseconds
-						int short_gap = 200;	// Length of Gap Between dots/dashes
-						int medium_gap = 500;	// Length of Gap Between Letters
-						int long_gap = 1000;	// Length of Gap Between Words
-						long[] pattern = {
-							0, 	// Start immediately
-							dot, short_gap, dot, short_gap, dot, 	// s
-							medium_gap,
-							dash, short_gap, dash, short_gap, dash, // o
-							medium_gap,
-							dot, short_gap, dot, short_gap, dot, 	// s
-							long_gap
-						};*/		
 		vibrator.vibrate(pattern, -1);
 	}
 
--- src/iching/android/activities/DivinationSMS.java
+++ src/iching/android/activities/DivinationSMS.java
@@ -2,16 +2,106 @@ package iching.android.activities;
 
 import iching.android.R;
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract.CommonDataKinds.Phone;
+import android.provider.ContactsContract.Contacts;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
 
 public class DivinationSMS extends Activity
 {
+
+	private static final int CONTACT_PICKER_RESULT = 1;
+	private EditText recipientNumber;
+	private EditText smsBody;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.divination_sms);
+		View browseButton = findViewById(R.id.browse_contacts);
+		recipientNumber = (EditText) findViewById(R.id.sms_number);
+		smsBody = (EditText) findViewById(R.id.sms_content);
+		browseButton.setOnClickListener(new OnClickListener()
+		{
+			@Override
+			public void onClick(View view)
+			{
+				launchContactPicker(view);
+			}
+		});
+		View sendSms = findViewById(R.id.send_sms);
+		sendSms.setOnClickListener(new OnClickListener()
+		{
+			
+			@Override
+			public void onClick(View v)
+			{
+		        SmsManager sms = SmsManager.getDefault();
+		        String number = recipientNumber.getText().toString();
+		        String body = smsBody.getText().toString();
+		        if(number.trim().length() == 0)
+		        {
+		        	
+		        }
+		        else if(body.trim().length() == 0)
+		        {
+		        	
+		        }
+		        else
+		        {
+		        	sms.sendTextMessage(number, null, body, null, null);
+		        	finish();
+		        }
+			}
+		});
+	}
+	
+	private void launchContactPicker(View view) {
+	    Intent contactPickerIntent = new Intent(Intent.ACTION_PICK, Contacts.CONTENT_URI);
+	    startActivityForResult(contactPickerIntent, CONTACT_PICKER_RESULT);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		if(resultCode == RESULT_OK)
+		{
+			switch (requestCode)
+			{
+				case CONTACT_PICKER_RESULT:
+					getContactMobileNumber(data);
+					break;
+				default:
+					break;
+			}
+		}
+	}
+
+	private void getContactMobileNumber(Intent data)
+	{
+		String contactId = data.getData().getLastPathSegment();
+		Cursor phones = getContentResolver().query(Phone.CONTENT_URI, null, Phone.CONTACT_ID + " = " + contactId, null, null);
+		String number = null;
+		while (phones.moveToNext()) {
+		    number = phones.getString(phones.getColumnIndex(Phone.NUMBER));
+		    int type = phones.getInt(phones.getColumnIndex(Phone.TYPE));
+		    switch (type) {
+		        case Phone.TYPE_HOME:
+		            break;
+		        case Phone.TYPE_MOBILE:
+		        	recipientNumber.setText(number);
+		            break;
+		        case Phone.TYPE_WORK:
+		            break;
+		        }
+		}
+		phones.close();
 	}
 	
 }
